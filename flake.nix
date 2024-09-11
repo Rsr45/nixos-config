59885803e44da16b75280edfa5d875755d6aaf9e
Rsr45 <efe45676@gmail.com>
2024-09-12 00:00:08 +0300

    change indent to 4

--- flake.nix
+++ flake.nix
@@ -1,40 +1,42 @@
 {
-  description = "A very basic flake";
+    description = "Property of CCP";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgsveryold.url = "github:nixos/nixpkgs?ref=nixos-21.11";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        nixpkgs-24-05.url = "github:nixos/nixpkgs/nixos-24.05";
+        nixpkgs-21-11.url = "github:nixos/nixpkgs/nixos-21.11";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        stylix.url = "github:danth/stylix";
     };
 
-    stylix.url = "github:danth/stylix";
-  };
-
-  outputs = { self, nixpkgs, ... } @ inputs:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        allowUnfree = true;
-      };
+    outputs = { self, nixpkgs, ... } @ inputs:
+    let
+        system = "x86_64-linux";
+        pkgs = import nixpkgs {
+            inherit system;
+            config = {
+                allowUnfree = true;
+            };
+        };
+    in
+    {
+        nixosConfigurations = {
+            apocrypha = nixpkgs.lib.nixosSystem {
+                specialArgs = { inherit inputs system; };
+                modules = [
+                    ./hosts/desktop/apocrypha/configuration.nix
+                ];
+            };
+            nixos = nixpkgs.lib.nixosSystem {
+                specialArgs = { inherit inputs system; };
+                modules = [
+                    ./hosts/vm/nixos/configuration.nix
+                    inputs.stylix.nixosModules.stylix
+                ];
+            };
+        };
     };
-  in
-  {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = [];
-    };
-  nixosConfigurations = {
-     nixos = nixpkgs.lib.nixosSystem {
-       specialArgs = { inherit inputs system; };
-       modules = [
-         ./hosts/fangyuan/configuration.nix
-         inputs.stylix.nixosModules.stylix
-        ];
-      };
-    };
-  };
 }
