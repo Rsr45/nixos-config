086b721d0c45ac24d9b6841eebec1b739548c352
Rsr45 <efe45676@gmail.com>
2024-09-12 22:26:20 +0300

    organized a bit

--- hosts/desktop/apocrypha/home.nix
+++ hosts/desktop/apocrypha/home.nix
@@ -72,6 +72,19 @@
         };
     };
 
+    programs.vim = {
+        enable = true;
+        plugins = with pkgs.vimPlugins [
+            vim-airline
+        ];
+        settings = {
+            background = "dark";
+        };
+        extraConfig = ''
+            set mouse=a
+        '';
+    };
+
     # This value determines the Home Manager release that your configuration is
     # compatible with. This helps avoid breakage when a new Home Manager release
     # introduces backwards incompatible changes.
