42836649d63b39961a4f7c0f92e900e740db612f
Rsr45 <148650240+Rsr45@users.noreply.github.com>
2025-02-12 18:55:54 +0300

    Update configuration.nix
    Flatpak

--- hosts/desktop/apocrypha/configuration.nix
+++ hosts/desktop/apocrypha/configuration.nix
@@ -200,6 +200,15 @@
     };
   };
 
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
   # # File Explorer
   programs = {
     thunar = {
