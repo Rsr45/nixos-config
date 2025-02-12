c0bb137aa0d887e4859615520e7d3d2ad71360dc
Rsr45 <148650240+Rsr45@users.noreply.github.com>
2025-02-12 17:35:48 +0300

    Update nvf.nix

--- hosts/desktop/apocrypha/modules/nvf.nix
+++ hosts/desktop/apocrypha/modules/nvf.nix
@@ -1,1 +1,23 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}; {
 
+  imports = [
+    inputs.nvf.homeManagerModules.default
+  ];
+
+  programs.nvf = {
+    enable = true;
+    settings = {
+      vim.viAlias = false;
+      vim.vimAlias = true;
+      vim.lsp = {
+        enable = true;
+      };
+    };
+  };
+
+}
