869f06b44ecd74f2bbb90267e465c6afe79d9985
Rsr45 <efe45676@gmail.com>
2025-03-10 23:42:49 +0300

    s

--- hosts/desktop/apocrypha/configuration.nix
+++ hosts/desktop/apocrypha/configuration.nix
@@ -6,6 +6,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./modules/steam.nix
     inputs.home-manager.nixosModules.home-manager
   ];
 
@@ -473,51 +474,6 @@
       # dsda-launcher # DSDA-DOOM Launcher.
       # eternity # DOOM Source Port.
     ];
-  };
-
-  # # Steam
-  programs = {
-    gamescope = {
-      enable = true;
-      capSysNice = true;
-    };
-    steam = {
-      enable = true;
-      package = pkgs.steam.override {
-        extraPkgs = pkgs:
-          with pkgs; [
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXinerama
-            xorg.libXScrnSaver
-            libpng
-            libpulseaudio
-            libvorbis
-            stdenv.cc.cc.lib
-            libkrb5
-            keyutils
-          ];
-      };
-      remotePlay = {
-        openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      };
-      dedicatedServer = {
-        openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-      };
-      localNetworkGameTransfers = {
-        openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-      };
-      gamescopeSession = {
-        enable = true;
-        args = [
-          "-r 144"
-          "-S stretch"
-        ];
-      };
-    };
-    gamemode = {
-      enable = true;
-    };
   };
 
   fonts.fontDir.enable = true;
